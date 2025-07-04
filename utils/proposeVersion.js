bff3e0944a5b9e10b8aa56dccb56d705d591f9ea
Maksim Kiselev <maximkiselevrossia@gmail.com>
2025-07-04 19:53:20 +0200

    Update proposeVersion.js

--- utils/proposeVersion.js
+++ utils/proposeVersion.js
@@ -27,11 +27,11 @@ async function run() {
     ]
 
     const frontProof = new ethers.Contract(FRONT_PROOF_CONTRACT_ADDRESS, abi, wallet);
-    const estimatedGasLimit = 500000; //await frontProof.proposeVersion.estimateGas(PROJECT_ID, CID, VERSION_NAME);
+    const estimatedGasLimit = 500000n; //await frontProof.proposeVersion.estimateGas(PROJECT_ID, CID, VERSION_NAME);
     const txUnsigned = await frontProof.proposeVersion.populateTransaction(PROJECT_ID, CID, VERSION_NAME);
     
     //txUnsigned.nonce = await wallet.getNonce("pending");
-    txUnsigned.gasLimit = await wallet.estimateGas(tx); //estimatedGasLimit;
+    txUnsigned.gasLimit = estimatedGasLimit;
     txUnsigned.chainId = await wallet.provider.getNetwork();//11155111; // chainId 11155111 for Sepolia
     //txUnsigned.gasPrice = await provider.getGasPrice();
 
