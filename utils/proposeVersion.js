6fda7e9427778257a54d397d483dab97f721125c
Maksim Kiselev <maximkiselevrossia@gmail.com>
2025-07-04 18:27:09 +0200

    Updated script

--- utils/proposeVersion.js
+++ utils/proposeVersion.js
@@ -2,6 +2,8 @@ import { JsonRpcProvider } from 'ethers';
 
 async function run() {
     const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+    const CID = process.env.CID;
+    const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
     // Connect to the Ethereum network
     const provider = new JsonRpcProvider("https://worldchain-sepolia.g.alchemy.com/v2/" + ALCHEMY_API_KEY);
@@ -10,7 +12,11 @@ async function run() {
     const blockNumber = "latest";
     const block = await provider.getBlock(blockNumber);
 
-    console.log("current block #", block);
+    const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
+
+    console.log("current block data", block);
+    console.log("connected manager wallet", await wallet.getAddress());
+    console.log("CID", CID);
 }
 
 run();
