c50085d9d1d278175edcc8418d94c9a038982d6c
Maksim Kiselev <maximkiselevrossia@gmail.com>
2025-07-04 19:32:18 +0200

    Update proposeVersion.js

--- utils/proposeVersion.js
+++ utils/proposeVersion.js
@@ -33,7 +33,7 @@ async function run() {
     txUnsigned.chainId = 11155111; // chainId 11155111 for Sepolia
     txUnsigned.gasLimit = estimatedGasLimit;
     //txUnsigned.gasPrice = await provider.getGasPrice();
-    txUnsigned.nonce = await provider.getTransactionCount(walletAddress);
+    txUnsigned.nonce = await wallet.getNonce();
 
     console.log("txUnsigned", txUnsigned);
 
